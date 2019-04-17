00cc1f96b3a5c9e7185e5273b0cd615e36bfc006
Raine Wallace <rainebeaudubs@gmail.com>
2019-04-16 20:33:04 -0400

    Added some routes and navLinks, navigation is imported and rendering.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 
+
 import Authenticate from './components/auth/Authentication';
+import Navigation from './components/Navigation';
+import Students from './components/students/Students';
 
 const App = Authenticate(
   class App extends Component {
@@ -9,6 +13,14 @@ const App = Authenticate(
       return (
         <div className="App">
           <h1>Hai, frens!</h1>
+          <Route 
+          path='/' 
+          component={Navigation}
+        />
+          <Route 
+            path='/students'
+            component={Students}
+          />
         </div>
       );
     }
---
+++ client/src/components/students/Students.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Students extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hi! I'm going to be the student list!</h1>
+      </div>
+    )
+  }
+}
+
+export default Students;
